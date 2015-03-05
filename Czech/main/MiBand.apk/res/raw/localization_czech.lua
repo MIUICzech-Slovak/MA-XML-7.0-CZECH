77c0a5e564db467eadbbf74691149e76df51d397
Jiří Roubalík <jiriroubalik@gmail.com>
2015-03-05 18:10:01 +0100

    MiBand 1.3.312

--- Czech/main/MiBand.apk/res/raw/localization_czech.lua
+++ Czech/main/MiBand.apk/res/raw/localization_czech.lua
@@ -220,6 +220,8 @@ localization_Czech_table = {
     manual_lazy_subtitle_used="The streak will be added one if you hit the goal later.",
     manual_fail_ytd_title = "Sorry, the streak broken yesterday.",
     manual_fail_ytd_subtitle = "Your streak until day before yesterday is %d.",
+    unlock_password_hint="Nastavte zámek telefonu pro vaši bezpečnost.",
+    unlock_password_hint_info="MiBand je nejlepší pro odemknutí.",
 
     get_distance_format="celkem %d m",
     activity_walk_format="%s Chůze %d kroků %s",
